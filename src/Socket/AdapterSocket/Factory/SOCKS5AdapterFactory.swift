ea5bcfedb3464911e834a2d09b14d0c00789d67a
mac <mac@macdeMacBook-Pro-2.local>
2018-06-17 12:55:23 +0800

    新增了socks5的用户名密码验证模式

--- src/Socket/AdapterSocket/Factory/SOCKS5AdapterFactory.swift
+++ src/Socket/AdapterSocket/Factory/SOCKS5AdapterFactory.swift
@@ -1,11 +1,12 @@
 import Foundation
 
 /// Factory building SOCKS5 adapter.
-open class SOCKS5AdapterFactory: ServerAdapterFactory {
-    override public init(serverHost: String, serverPort: Int) {
-        super.init(serverHost: serverHost, serverPort: serverPort)
+open class SOCKS5AdapterFactory: SOCKS5AuthenticationAdapterFactory {
+    
+    required public init(serverHost: String, serverPort: Int, authName: String?, authPswd: String?) {
+        super.init(serverHost: serverHost, serverPort: serverPort, authName: authName, authPswd: authPswd)
     }
-
+    
     /**
      Get a SOCKS5 adapter.
 
@@ -14,8 +15,9 @@ open class SOCKS5AdapterFactory: ServerAdapterFactory {
      - returns: The built adapter.
      */
     override open func getAdapterFor(session: ConnectSession) -> AdapterSocket {
-        let adapter = SOCKS5Adapter(serverHost: serverHost, serverPort: serverPort)
+        let adapter = SOCKS5Adapter(serverHost: serverHost, serverPort: serverPort, authName: authName, authPswd: authPswd)
         adapter.socket = RawSocketFactory.getRawSocket()
         return adapter
     }
 }
+
